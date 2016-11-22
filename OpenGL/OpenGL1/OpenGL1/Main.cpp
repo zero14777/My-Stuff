c64378fd77ab1e0795f04aa6b8ccfdf295150763
Pbgreerb <pbgreerb@gmail.com>
2016-11-21 17:32:16 -0800

    setup openGL project

---
+++ OpenGL/OpenGL1/OpenGL1/Main.cpp
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+using namespace std;
+
+void changeViewPort(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLineWidth(2.5);
+	glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(15, 0, 0);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("View");
+
+	glutReshapeFunc(changeViewPort);
+	glutDisplayFunc(render);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+
+	glutMainLoop();
+	return 0;
+}
