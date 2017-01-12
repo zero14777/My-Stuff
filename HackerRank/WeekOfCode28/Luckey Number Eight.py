6cd80bb851f5641573d8429aaca1ec5fecba5ecf
Pbgreerb <pbgreerb@gmail.com>
2017-01-12 12:40:05 -0800

    improvement on day 3

--- HackerRank/WeekOfCode28/Luckey Number Eight.py
+++ HackerRank/WeekOfCode28/Luckey Number Eight.py
@@ -10,36 +10,51 @@
 
 import sys
 
-
 n = int(input().strip())
 number = input().strip()
+# your code goes here
+#keep track of all possible subsequences with a third least sig digit that is odd or even
+odds = 0
+evens = 0
 
-result = 0
+#keep track of how many subsequences can be made using the various even digits
+acc08 = 0
+acc6 = 0
+acc4 = 0
+acc2 = 0
 
-if n >= 1:
-    for a in number:
-        if ((ord(a) - 48) % 8) == 0:
-            result = result + 1
+#keep track of digit position we are looking at
+pos = 0
 
-if n >= 2:
-    for a in range(0, (len(number)-1)):
-        for b in range(a+1, len(number)):
-            seq = (ord(number[a]) -48) * 10
-            seq += (ord(number[b]) -48)
-            if (seq % 8) == 0:
-                result = result + 1
+output = 0
 
-#Way too slow
-if n >= 3:
-    for c in range(len(number)-1, 1, -1):
-        if (ord(number[c]) - 48) % 2 == 1:
-            continue
-        for b in range(c-1, 0, -1):
-            for a in range(b-1, -1, -1):
-                seq = (ord(number[a]) -48) * 100
-                seq += (ord(number[b]) -48) * 10
-                seq += (ord(number[c]) -48)
-                if (seq % 8) == 0:
-                    result = (result + pow (2, a)) % (1000000007)
-        
-print(result)
+for c in number:
+    if int(c) == 0 or int(c) == 8:
+        output = output + 1 + acc08
+        acc08 = acc08 + 1 + evens
+        acc4 = acc4 + odds
+    elif int(c) == 6:
+        output = output + acc6
+        acc08 = acc08 + odds
+        acc4 = acc4 + 1 + evens
+    elif int(c) == 4:
+        output = output + acc4
+        acc08 = acc08 + 1 + evens
+        acc4 = acc4 + odds
+    elif int(c) == 2:
+        output = output + acc2
+        acc08 = acc08 + odds
+        acc4 = acc4 + 1 + evens
+    elif int(c) == 3 or int(c) == 7:
+        acc2 = acc2 + 1 + evens
+        acc6 = acc6 + odds
+    else:#1 or 5 or 9
+        acc2 = acc2 + odds
+        acc6 = acc6 + 1 + evens
+    if int(c) % 2 == 1:
+        odds = odds + pow(2, pos)
+    else:
+        evens = evens + pow(2, pos)
+    pos = pos + 1
+
+print(output%1000000007)
