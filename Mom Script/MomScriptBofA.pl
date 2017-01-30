6c4bef6d46b00e15bd955ba3581641f05c918745
Pbgreerb <pbgreerb@gmail.com>
2017-01-29 16:54:47 -0800

    added check for some wierd formatting that can occur when pdf converted to text

--- Mom Script/MomScriptBofA.pl
+++ Mom Script/MomScriptBofA.pl
@@ -22,6 +22,7 @@ open(my $fh2, ">", $temp2)
 	or die "Could not make file '$temp2' $!";
 
 my $tableCount = 0;
+my $transactionsInQueue = 0;
 
 while (my $outerline = <$data>) {
 	chomp $outerline;
@@ -34,11 +35,13 @@ while (my $outerline = <$data>) {
 			chomp $line;
 			if($line =~ m/^Total.*$/) {
 				print $fh1 "\n";
+				$transactionsInQueue = $transactionsInQueue+1;
 				last;
 			}
 			
 			if($line =~ m/^Amount.*$/) {
 				print $fh1 "\n";
+				$transactionsInQueue = $transactionsInQueue+1;
 				$outerline = $line;
 				last;
 			}
@@ -47,12 +50,11 @@ while (my $outerline = <$data>) {
 			if ($line =~ m/^(\d\d\/\d\d\/\d\d)(.*)$/) {
 				if($gettingTransaction) {
 					print $fh1 "\n";
+					$transactionsInQueue = $transactionsInQueue+1;
 				}
 				my $date = $1;
-				$date = "$date";
 				my $desc = $2;
 				$desc =~ s/[",]//; #" chars would mess up the generated csv file
-				$desc = "$desc";
 				print $fh1 "$date,$desc";
 				$gettingTransaction = 1;
 			} elsif (!($line eq "") and $gettingTransaction) {
@@ -64,31 +66,100 @@ while (my $outerline = <$data>) {
 	# Get Amounts
 	if($outerline =~ m/^Amount.*$/ and $tableCount > 0) {# Be aware of picking up too many amounts
 		$tableCount = $tableCount - 1;
+		my $end_early = 0;
+		
+		if ($outerline =~ m/\$/) {# check for $ on first line
+			$outerline =~ s/-?\$.*$//;
+			$end_early = 1;
+		}
+		
 		my @amounts = split " ", $outerline;
+		
 		foreach my $amount (@amounts) {
 			if (!($amount eq "Amount")) {
 				$amount =~ s/[",]//;
 				print $fh2 "$amount\n";
+				$transactionsInQueue = $transactionsInQueue - 1;
 			}
 		}
-		my $gettingAmounts = 1;
-		while ($gettingAmounts) {
-			my $line = <$data>;
-			chomp $line;
-			
-			if($line eq "continued on the next page") {
+		
+		if ($end_early) {
+			#do nothing
+		} elsif ($transactionsInQueue == 0) {
+			my $line;
+			my $first = 1;
+			while ($line = <$data>) {
+				chomp $line;
+				
+				if($line eq "") {
+					next;
+				}
+				
+				if (!($line =~ m/^(\d\d\/\d\d\/\d\d)(.*)$/)) {
+					$first = -1;
+				}
 				last;
 			}
 			
-			if ($line =~ m/\$/) {
-				$gettingAmounts = 0;
-				$line =~ s/-?\$.*$//;
+			while (1) {
+				if ($first == 0) {
+					$line = <$data>;
+					chomp $line;
+					if ($line eq "") {
+						next;
+					}
+				} elsif ($first == 1) {
+					$first = 0;
+				} else {
+					last;
+				}
+				
+				if(!($line =~ m/^(\d\d\/\d\d\/\d\d)(.*)$/)){ #$line =~ m/^Total (.*)$/) {
+					last;
+				} else {
+					my $date = $1;
+					my $desc = $2;
+					$desc =~ s/[",]//; #" chars would mess up the generated csv file
+					print $fh1 "$date,$desc";
+				}
+				
+				while(1){
+					$line = <$data>;
+					chomp $line;
+					
+					if ($line =~ m/^-?[\d,]*\.\d\d$/) {
+						#$line =~ s/-?\$.*$//;
+						$line =~ s/[",]//;
+						print $fh2 "$line\n";
+						print $fh1 "\n";
+						last;
+					} elsif (!($line eq "")) {
+						$line =~ s/[",]//;
+						print $fh1 " $line";
+					}
+				}
 			}
-			
-			my @amounts = split " ", $line;
-			foreach my $amount (@amounts) {
-				$amount =~ s/[",]//;
-				print $fh2 "$amount\n";
+		} else { # run through amounts like normal
+			my $gettingAmounts = 1;
+			while ($gettingAmounts) {
+				my $line = <$data>;
+				chomp $line;
+				
+				if($line eq "continued on the next page") {
+					last;
+				}
+				
+				if ($line =~ m/\$/) {
+					$gettingAmounts = 0;
+					$line =~ s/-?\$.*$//;
+				}
+				
+				my @amounts = split " ", $line;
+				foreach my $amount (@amounts) {
+					$amount =~ s/[",]//;
+					print $fh2 "$amount\n";
+					$transactionsInQueue = $transactionsInQueue - 1;
+				}
 			}
 		}
 	}
@@ -96,8 +167,8 @@ while (my $outerline = <$data>) {
 
 close($fh1);
 close($fh2);
-open(my $fh1, "<", $temp1);
-open(my $fh2, "<", $temp2);
+open($fh1, "<", $temp1);
+open($fh2, "<", $temp2);
 
 my $output = 'output.csv';
 open(my $fhout, ">", $output);
