831b54d7bf1a43b9c601de8461302e964b5871d1
Pbgreerb <pbgreerb@gmail.com>
2017-02-09 19:15:46 -0800

    practice

---
+++ HackerRank/Cracking the Code Interview/Balancing Brackets.cpp
@@ -0,0 +1,68 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+bool is_balanced(string expression) {
+    vector<char> myStack;
+    for(int i = 0; i < expression.size(); i++){
+        char next = expression.at(i);
+        if (next == ')') {
+            if (myStack.size() == 0 || myStack.back() != '(') {
+                return false;
+            }
+            myStack.pop_back();
+        } else if (next == '}') {
+            if (myStack.size() == 0 || myStack.back() != '{') {
+                return false;
+            }
+            myStack.pop_back();
+        } else if (next == ']') {
+            if (myStack.size() == 0 || myStack.back() != '[') {
+                return false;
+            }
+            myStack.pop_back();
+        } else {
+            myStack.push_back(next);
+        }
+    }
+    
+    if (myStack.size() == 0) {
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        string expression;
+        cin >> expression;
+        bool answer = is_balanced(expression);
+        if(answer)
+            cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
